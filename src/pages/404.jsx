adde74853fcbfe122ecd30720a0f67cad93e3e84
Alan Ibatullin <mr.juneiak@gmail.com>
2022-07-05 04:29:11 +0300

    404 fix

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
-import { navigate } from 'gatsby'
+import React, { useEffect } from "react";
+import { navigate } from "gatsby";
 
-const NotFoundPage = () => {
-  return navigate('/')
+export default function NotFoundPage() {
+  useEffect(() => {
+    navigate("/"); // redirecting to home page
+  }, []);
+
+  return (
+    <div>
+      <h1>(404) NotFound Page</h1>
+    </div>
+  );
 }
-
-export default NotFoundPage
